529d30dcc9d69aead87ce0d562b2fbf20f84af5c
xyrovoiddev0 <297034007+xyrovoiddev0@users.noreply.github.com>
2026-08-22 17:13:08 -0300

    Atualizar o tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'myka-red': '#FF0000',
+        'myka-black': '#000000',
+        'myka-gray': '#888888',
+      },
+      fontFamily: {
+        'archivo': ['"Archivo Black"', 'sans-serif'],
+        'inter': ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
