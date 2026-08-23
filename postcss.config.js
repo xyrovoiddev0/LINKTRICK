8857ea6843288db917577e487ed1f944678c1988
xyrovoiddev0 <297034007+xyrovoiddev0@users.noreply.github.com>
2026-08-22 22:12:48 -0300

    Atualizar o postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,19 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'myka-red': '#FF0000',
+        'myka-black': '#000000',
+        'myka-gray': '#888888',
+        'myka-dark': '#0f0f0f',
+      },
+      fontFamily: {
+        'mont': ['Montserrat', 'sans-serif'],
+        'mono': ['Space Mono', 'monospace'],
+      },
+    },
   },
+  plugins: [],
 };
